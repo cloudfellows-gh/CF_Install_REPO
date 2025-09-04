52508e64646278bed827ecbeaa4edbe95382eca1
Mihai Monte <Mihai@limon-it.nl>
2025-09-04 09:49:13 +0200

    Add user to FSLogix ODFC Exclude List with group creation check

--- AIB-Install-Scripts/aib-winget-install-apps.ps1
+++ AIB-Install-Scripts/aib-winget-install-apps.ps1
@@ -115,3 +115,25 @@ if ($groupMembers -notcontains $userToAdd) {
 } else {
     Write-Log "'$userToAdd' is already a member of group '$groupName'"
 }
+################################################################
+# Add user to local group for FSLogix Office profile exclusion
+################################################################
+$groupName = "FSLogix ODFC Exclude List"
+$userToAdd = "cfuser1815"
+
+# Check if group exists, create if it doesn't
+if (-not (Get-LocalGroup -Name $groupName -ErrorAction SilentlyContinue)) {
+    New-LocalGroup -Name $groupName -Description "Users excluded from FSLogix profile management"
+    Write-Log "Created local group '$groupName'"
+} else {
+    Write-Log "Local group '$groupName' already exists"
+}
+
+# Check if user is in group
+$groupMembers = Get-LocalGroupMember -Group $groupName | Select-Object -ExpandProperty Name
+if ($groupMembers -notcontains $userToAdd) {
+    Add-LocalGroupMember -Group $groupName -Member $userToAdd
+    Write-Log "Added '$userToAdd' to group '$groupName'"
+} else {
+    Write-Log "'$userToAdd' is already a member of group '$groupName'"
+}
